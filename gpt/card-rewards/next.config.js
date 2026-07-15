1f90254c15806e44cb53dcfa4395c5e76efdc957
NIKO <artheshe@NIKO-MACBOOK14.local>
2026-07-15 12:09:05 +0800

    Remove accidentally add

--- gpt/card-rewards/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'standalone',
-  experimental: {
-    // App router is default in Next.js 15
-  },
-};
-
-module.exports = nextConfig;
